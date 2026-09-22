34adfebf101f232a17176ac19fbec557d5f2ebd7
shelken <shelken.pxk@gmail.com>
2026-09-22 18:46:10 +0800

    chore(workbuddy): 0.2.5 -> 0.2.6

--- plugins/workbuddy/main.go
+++ plugins/workbuddy/main.go
@@ -316,7 +316,7 @@ func handleMethod(method string, request []byte) ([]byte, error) {
 			SchemaVersion: pluginabi.SchemaVersion,
 			Metadata: pluginapi.Metadata{
 				Name:             "WorkBuddy",
-				Version:          "0.2.5",
+				Version:          "0.2.6",
 				Author:           "shelken",
 				GitHubRepository: "https://github.com/shelken/cpa-plugins",
 				ConfigFields: []pluginapi.ConfigField{
